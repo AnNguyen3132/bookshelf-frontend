67dbd12080ba3b973733e4f54c3b64b28eb851bf
Tam Doan <sytamdoan@gmail.com>
2025-06-04 22:50:48 -0500

    frontend able to talk to backend

--- src/views/Recommendations.vue
+++ src/views/Recommendations.vue
@@ -1,12 +1,40 @@
+<script setup>
+import { onMounted } from 'vue'
+import { ref, computed } from "vue";
+import LLMServices from "../services/LLMServices.js";
+import OwnedBooksServices from "../services/OwnedBooksServices.js";
+const OwnedBooks = ref([])
+
+onMounted(async () => {
+  try {
+    getRecommendations()
+  } catch (error) {
+    console.error(error)
+  }
+});
+
+function getRecommendations() {
+  fetchOwnedBooks().then(() => {
+    LLMServices.getRecommendations(OwnedBooks.value)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+}
+
+async function fetchOwnedBooks() {
+  const response = await OwnedBooksServices.getOwnedBook()
+  OwnedBooks.value = response.data
+}
+
+</script>
+
 <template>
   <h1 class="title">Recommendations Page</h1>
 </template>
-
-<script>
-export default {
-  name: 'RecommendationsPageHeader'
-};
-</script>
 
 <style scoped>
 .title {
