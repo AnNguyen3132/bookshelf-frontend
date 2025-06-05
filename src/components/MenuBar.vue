8fd22e404413ea78b4ecff039b46934039c83cc7
Tam Doan <sytamdoan@gmail.com>
2025-06-04 23:57:56 -0500

    Logout button now takes you back to the login screen

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -72,7 +72,7 @@ export default {
           <v-list-item :to="{ name: 'UserDatabasePage' }" link>
             <v-list-item-title>User Database</v-list-item-title>
           </v-list-item>
-          <v-list-item :to="{ name: 'Recommendations' }" link>
+          <v-list-item :to="{ name: 'login' }" link>
             <v-list-item-title>Logout</v-list-item-title>
           </v-list-item>
         </v-list>
