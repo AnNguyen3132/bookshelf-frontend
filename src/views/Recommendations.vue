01f64c39bc27afe45f00b3881b0f1eaf9784be1e
Tam Doan <sytamdoan@gmail.com>
2025-06-05 00:44:40 -0500

    reverted Recommendations.vue page

--- src/views/Recommendations.vue
+++ src/views/Recommendations.vue
@@ -1,33 +1,12 @@
-<script setup>
-import { ref, onMounted } from "vue";
-import OwnedBooksServices from "../services/OwnedBooksServices.js";
-import LLMServices from "../services/LLMServices.js"
-
-const OwnedBooks = ref([])
-
-onMounted(async () => {
-  getRecommendations();
-});
-
-async function getRecommendations() {
-  try {
-    await fetchOwnedBooks();
-    const response = await LLMServices.getRecommendations(OwnedBooks.value);
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
-async function fetchOwnedBooks() {
-  const response = await OwnedBooksServices.getOwnedBook()
-  OwnedBooks.value = response.data.map(entry => entry.book.title);
-}
-
-</script>
-
 <template>
   <h1 class="title">Recommendations Page</h1>
 </template>
+
+<script>
+export default {
+  name: 'RecommendationsPageHeader'
+};
+</script>
 
 <style scoped>
 .title {
