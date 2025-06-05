907ff7a4ed8bd32ec17e06fed87884256025dfc8
Tam Doan <sytamdoan@gmail.com>
2025-06-04 23:31:04 -0500

    added formatting into the books given back on recommendation

--- src/views/Recommendations.vue
+++ src/views/Recommendations.vue
@@ -4,6 +4,7 @@ import { ref, computed } from "vue";
 import LLMServices from "../services/LLMServices.js";
 import OwnedBooksServices from "../services/OwnedBooksServices.js";
 const OwnedBooks = ref([])
+const recommendedBooks = ref([])
 
 onMounted(async () => {
   try {
@@ -14,10 +15,14 @@ onMounted(async () => {
 });
 
 function getRecommendations() {
+  console.log(OwnedBooks.value);
   fetchOwnedBooks().then(() => {
     LLMServices.getRecommendations(OwnedBooks.value)
       .then((response) => {
         console.log(response);
+        const raw = response.data.recommendations;
+        const jsonText = raw.replace(/```json\n?/, '').replace(/\n?```$/, '');
+        recommendedBooks.value = JSON.parse(jsonText);
       })
       .catch((error) => {
         console.log(error);
@@ -27,6 +32,7 @@ function getRecommendations() {
 
 async function fetchOwnedBooks() {
   const response = await OwnedBooksServices.getOwnedBook()
+  console.log("Fetched books:", response.data);
   OwnedBooks.value = response.data
 }
 
@@ -34,6 +40,28 @@ async function fetchOwnedBooks() {
 
 <template>
   <h1 class="title">Recommendations Page</h1>
+
+  <v-table>
+  <thead>
+    <tr>
+      <th class="text-left">Title</th>
+      <th class="text-left">Author</th>
+      <th class="text-left">Publisher</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="recommendedBook in recommendedBooks" :key="recommendedBook.book" class="mb-2">
+      <td class = "cursor-pointer" >{{recommendedBook.book}}</td>
+      <td class = "cursor-pointer" >{{recommendedBook.publisher }}</td>
+      <td class = "cursor-pointer" >{{recommendedBook.author }}</td>
+      <td>
+        <v-icon color="red" class="cursor-pointer" > mdi-pencil </v-icon>
+        | 
+        <v-icon color="red" class="cursor-pointer" > mdi-delete </v-icon>
+      </td>
+    </tr>
+  </tbody>
+</v-table>
 </template>
 
 <style scoped>
