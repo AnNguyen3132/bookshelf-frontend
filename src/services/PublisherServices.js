4f7fdbb6e10d31d196ee1f9c1d7e4a8313d0f21f
An Nguyen <annguyen3132@gmail.com>
2025-06-08 19:08:25 -0500

    Fill out services for Publisher

--- src/services/PublisherServices.js
+++ src/services/PublisherServices.js
@@ -4,4 +4,13 @@ export default {
   getPublishers() {
     return apiClient.get("Publishers");
   },
+  addPublisher(publisher) {
+    return apiClient.post("/Publisher/", publisher);
+  },
+  updatePublisher(publisherId, Publisher) {
+    return apiClient.put("/Publisher/" + publisherId, Publisher);
+  },
+  deletePublisher(publisherId) {
+    return apiClient.delete("/Publisher/" + publisherId)
+  }
 };
